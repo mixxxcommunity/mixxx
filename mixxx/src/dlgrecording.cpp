c31d6286b78d67208a4c1e40abdfa69112ea2603
Daniel Schürmann <daschuer@mixxx.org>
2011-07-02 22:30:47 +0200

    >add to Auto DJ top 2< added

--- mixxx/src/dlgrecording.cpp
+++ mixxx/src/dlgrecording.cpp
@@ -142,7 +142,8 @@ void DlgRecording::moveSelection(int delta) {
 
 void DlgRecording::toggleRecording(bool toggle)
 {
-    if (!m_pRecordingManager->isRecordingActive()) //If recording is enabled
+    Q_UNUSED(toggle);
+	if (!m_pRecordingManager->isRecordingActive()) //If recording is enabled
     {
         //pushButtonRecording->setText(tr("Stop Recording"));
         m_pRecordingManager->startRecording();
