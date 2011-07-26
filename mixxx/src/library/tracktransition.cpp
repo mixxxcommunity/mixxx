a7b7b8397f8c8a3481fae9e49765d02500a1dfac
Tom Mast <thomasomast@gmail.com>
2011-07-26 04:12:41 -0400

    Fixed crossfade issue

--- mixxx/src/library/tracktransition.cpp
+++ mixxx/src/library/tracktransition.cpp
@@ -33,9 +33,17 @@ void TrackTransition::transition(QString groupA, QString groupB, TrackPointer tr
     // Crossfading from Player 1 to Player 2
     if (groupA == "[Channel1]" && groupB == "[Channel2]") {
 
+        // Find the FadeOut point position and crossfade length as ratios
+        // of the whole track. posFadeEnd is then the position where the crossfade
+        // will stop.
         float sampleThreshold = trackA->getFadeOut() / m_pCOTrackSamples1->get();
         float fadeLengthRatio = (float) m_iFadeLength / trackA->getDuration();
         float posFadeEnd = sampleThreshold + fadeLengthRatio;
+
+        // Check if our crossfade would stop past the end of the track
+        if (posFadeEnd > 1.0) {
+            posFadeEnd = 1.0;
+        }
 
         float crossfadeValue = -1.0f + 2*(m_pCOPlayPos1->get()-sampleThreshold) / (posFadeEnd - sampleThreshold);
         m_pCOCrossfader->slotSet(crossfadeValue);
@@ -44,11 +52,19 @@ void TrackTransition::transition(QString groupA, QString groupB, TrackPointer tr
     // Crossfading from Player2 to Player 1
     if (groupA == "[Channel2]" && groupB == "[Channel1]") {
 
+        // Find the FadeOut point position and crossfade length as ratios
+        // of the whole track. posFadeEnd is then the position where the crossfade
+        // will stop.
         float sampleThreshhold = trackA->getFadeOut() / m_pCOTrackSamples2->get();
         float fadeLengthRatio = (float) m_iFadeLength / trackA->getDuration();
         float posFadeEnd = sampleThreshhold + fadeLengthRatio;
 
-        float crossfadeValue = -1.0f + 2*(m_pCOPlayPos2->get()-sampleThreshhold) / (posFadeEnd - sampleThreshhold);
+        // Check if our crossfade would stop past the end of the track
+        if (posFadeEnd > 1.0) {
+            posFadeEnd = 1.0;
+        }
+
+        float crossfadeValue = 1.0f - 2*(m_pCOPlayPos2->get()-sampleThreshhold) / (posFadeEnd - sampleThreshhold);
         m_pCOCrossfader->slotSet(crossfadeValue);
 
     }
